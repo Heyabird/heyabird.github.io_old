580fbfbcd659e7d27ca5b24f2228a3e515a4ca70
Heya Kwon <heyabird@gmail.com>
2020-04-22 11:00:09 -0700

    changed a few things

--- jQuery.js
+++
@@ -1,9 +0,0 @@
-// $(document).scroll(function () {
-//     var y = $(this).scrollTop();
-//     if (y > 800) {
-//         $('.skillsbox').fadeIn();
-//     } else {
-//         $('.skillsbox').fadeOut();
-//     }
-
-// });
