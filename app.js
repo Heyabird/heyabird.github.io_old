c201a0ccec0896d5c7e40796258efc3c5247518b
heya <heyabird@gmail.com>
2020-10-16 19:30:12 -0700

    fix bug on the emoji becoming ? before 🌈

--- app.js
+++ app.js
@@ -1,60 +1,81 @@
+function checkUTF8(text) {
+  var utf8Text = text;
+  try {
+      utf8Text = decodeURIComponent(encodeURIComponent(text));
+  }catch(e) {
+      // console.log(e.message); // URI malformed
+      return false
+      // This exception means text is utf-8
+  }   
+  return true; // returned text is always utf-8
+}
+
 var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 0o00;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 0o00;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  // console.log(this.txt)
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  if (this.isDeleting) { delta /= 2; }
 
-    if (this.isDeleting) { delta /= 2; }
+  if(checkUTF8(this.txt)) {
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  } else {
+    console.log(this.txt)
+  }
 
-    // if (!this.isDeleting && this.txt === fullTxt) {
-    // delta = this.period;
-    // this.isDeleting = true;
-    // } else if (this.isDeleting && this.txt === '') {
-    // this.isDeleting = false;
-    // this.loopNum++;
-    // delta = 500;
-    // }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
+  setTimeout(function() {
+  that.tick();
+  }, delta);
 };
 
 window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
 
+// window.onload = function() {
+//   var i = 0;
+//   var txt = 'Lorem ipsum typing effect!'; /* The text */
+//   var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+//   function typeWriter() {
+//     if (i < txt.length) {
+//       document.getElementById("demo").innerHTML += txt.charAt(i);
+//       i++;
+//       setTimeout(typeWriter, speed);
+//     }
+//   }
+// }
 
 
 
